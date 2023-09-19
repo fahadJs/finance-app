ce0d96e509f694a0182c994cc99a7837c5d1bff5
fahad <fahad.js@yahoo.com>
2023-09-19 18:43:05 +0500

    Development Branch

--- src/main/java/github/fahadjs/financeapp/FinanceAppApplication.java
+++ src/main/java/github/fahadjs/financeapp/FinanceAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinanceAppApplication {
 
+//	Github Development Branch
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceAppApplication.class, args);
 	}
